904cd6351ab12d9e3f118142b71afd67db07b67c
Talha Junaid <talhajunaid65@gmail.com>
2023-03-04 16:39:24 +0500

    Permitted domain_name column for a company.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -65,6 +65,6 @@ class CompaniesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def company_params
-      params.require(:company).permit(:name)
+      params.require(:company).permit(:name, :domain_name)
     end
 end
