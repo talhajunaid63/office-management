d306cb6f22fe65eaa926149c197cfc62e9f8e067
Talha Junaid <talhajunaid65@gmail.com>
2023-03-04 16:13:47 +0500

    Annonated mode.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,29 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  company_joining_date   :string
+#  dob                    :date
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  first_name             :string
+#  job_position           :string
+#  job_type               :string
+#  last_name              :string
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  role                   :string
+#  salary                 :float
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
