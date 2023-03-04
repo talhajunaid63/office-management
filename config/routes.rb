8bcc5a736aa192a097d95269e3ad87172940430a
Talha Junaid <talhajunaid65@gmail.com>
2023-03-04 16:43:18 +0500

    Added home page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
+  root "home#index"
+
   devise_for :users
   resources :companies
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
