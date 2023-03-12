9c872f14af4ddcb16a31125b772ed96b58b94add
Talha Junaid <talhajunaid65@gmail.com>
2023-03-12 17:56:22 +0500

    Integrated tailwind flowbite theme.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def sidebar_active_class(path)
+    return unless current_page?(path)
+
+    'bg-gray-100 dark:bg-gray-700'
+  end
 end
---
+++ app/javascript/custom/index.js
@@ -0,0 +1,6 @@
+import "flowbite/dist/flowbite.js";
+import "./sidebar";
+import "./charts";
+import "./dark-mode";
+
+// Have the courage to follow your heart and intuition.
